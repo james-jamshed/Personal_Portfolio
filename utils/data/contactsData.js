a38dec0ef835d8cdff496b0f1387e71f3bf51446
Jamshed Alam <jamshedalam1777@gmail.com>
2024-11-05 14:13:28 +0530

    Initial commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'jamshedalam0823@gmail.com',
+    phone: '+919939600823',
+    address: 'LUCKNOW, UTTAR PRADESH, INDIA ',
+    github: 'https://github.com/james-jamshed/',
+    facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    linkedIn: 'https://www.linkedin.com/in/james-jamshed/',
+    twitter: 'https://twitter.com/jamshedalam1777',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "said7388"
+}
---
+++ utils/data/educations.js
@@ -0,0 +1,21 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor Degree",
+    duration: "2021 - Present",
+    institution: `Dr. A. P. J. Abdul Kalam Technical University, Lucknow`
+
+  },
+  {
+    id: 2,
+    title: "Higher Secondary Certificate",
+    // duration: "2018 - 2020",
+    institution: "Kamla Roy Science College",
+  },
+  {
+    id: 3,
+    title: "Secondary School Certificate",
+    // duration: "2008 - 2018",
+    institution: "M M Memorial Urdu High School",
+  }
+]
